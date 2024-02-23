fdd20e8f859240c423c7b26bd509546d8a0e48f5
Jordan Jones <jmjones2463@gmail.com>
2024-02-23 13:58:41 -0800

    perf(min): update min js system for docsite

--- demo/index.js
+++ demo/index.js
@@ -1,15 +1,12 @@
-// import { expandedExample } from "../apiExamples/expanded";
-
-export function initAccordionIndexExamples(initCount) {
+export function initExamples(initCount) {
   initCount = initCount || 0;
 
   try {
-    // expandedExample();
   } catch {
     if (initCount <= 20) {
       // setTimeout handles issue where content is sometimes loaded after the functions get called
       setTimeout(() => {
-        initAccordionIndexExamples(initCount + 1);
+        initExamples(initCount + 1);
       }, 100);
     }
   }
