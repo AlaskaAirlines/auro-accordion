d203f3fd2f797ed7fd7ff4652ba10c1ea68f84f1
Jordan Jones <jmjones2463@gmail.com>
2024-09-30 14:28:21 -0700

    perf: refactor custom component registration config

--- index.js
+++ index.js
@@ -2,21 +2,8 @@ import { AuroAccordion } from './src/auro-accordion.js';
 import { AuroAccordionGroup } from './src/auro-accordion-group.js';
 import { AuroAccordionButton } from './src/auro-accordion-button.js';
 
-/* eslint-disable func-style */
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @param {Function} className - Class name to use for custom element.
- * @returns {void}
- */
-export function registerComponent(name, className) {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends className {});
-  }
-}
-
-registerComponent('auro-accordion', AuroAccordion);
-registerComponent('auro-accordion-group', AuroAccordionGroup);
-registerComponent('auro-accordion-button', AuroAccordionButton);
+RuntimeUtils.default.prototype.registerComponent('custom-accordion', AuroAccordion);
+RuntimeUtils.default.prototype.registerComponent('custom-accordion-group', AuroAccordionGroup);
+RuntimeUtils.default.prototype.registerComponent('custom-accordion-button', AuroAccordionButton);
